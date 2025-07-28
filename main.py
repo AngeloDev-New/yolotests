6a43cde1ba994389bdaf4d875943191df8cd5e50
Angelo <angelo.dev.santos@gmail.com>
2025-07-28 12:12:18 -0300

    subindo modelos

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # modelo pré-treinado
 
 # 0 = webcam padrão
-model.predict(source=0, show=True, conf=0.6)  # show=True exibe a janela com deteções ao vivo
+model.predict(source=0, show=True, conf=0.1)  # show=True exibe a janela com deteções ao vivo
